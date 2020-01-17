0ca858bdc638606384f250ccb3bf1dfc3be7ed50
Steven Nyanumba <stevennmb9@gmail.com>
2020-01-17 04:11:09 +0300

    Completed Lab 1

--- User.php
+++ User.php
@@ -14,11 +14,52 @@ class User implements crud
         $this->city_name = $city_name;
     }
 
-    public function setUserId(){
+    public function setUserId($user_id){
         $this->user_id = $user_id;
     }
+
+    public function getUserId(){
+        return $this->user_id;
+    }
+
+    public function save(){
+        $fn = $this->first_name;
+        $ln = $this->last_name;
+        $city = $this->city_name;
+        $res = mysql_query("INSERT INTO user(first_name,last_name,user_city)VALUES("$fn","$ln","$city")") or die("Error: ".mysql_error);
+        return $res;
+
+    }
+
+    public function readAll(){
+        
+        return null;
+    }
+
+    public function readUnique(){
+
+        return null;
+    }
+
+    public function search(){
+
+        return null;
+    }
+
+    public function update(){
+
+        return null;
+    }
+
+    public function removeOne(){
+
+        return null;
+    }
+
+    public function removeAll(){
+        return null;
+    }
+
 }
-
-
 
 ?>
--- lab1.php
+++ lab1.php
@@ -1,4 +1,25 @@
+<?php 
+ include_once "DBConnector.php";
+ include_once "user.php";
+ $con = new DBConnector;
 
+ if (isset($_POST['btn-save'])) {
+     $first_name = $_POST['first_name'];
+     $last_name = $_POST['last_name'];
+     $city = $_POST['city_name'];
+
+     $user = new User($first_name,$last_name,$city);
+     $res = $user->save();
+
+     if($res){
+         echo "Save operation was successful"
+     }
+     else{
+         echo "An error occured";
+     }
+ }
+
+?>
 
 
 <!DOCTYPE html>
@@ -22,7 +43,7 @@
                 <td><input type="text" name="city_name" placeolder="City Name" ></td>
             </tr>
             <tr>
-                <td><button type="submit"><strong>SAVE</strong></button></td>
+                <td><button type="submit" name="btn-save"><strong>SAVE</strong></button></td>
             </tr>
         </table>
     </form>
